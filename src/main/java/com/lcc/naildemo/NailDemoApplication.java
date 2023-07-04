3dba3d0c9fb6575f7f850cbccf24c513429abeb7
lcc <liuchuangchuang@yintatech.com>
2023-07-04 18:04:50 +0800

    first init

---
+++ src/main/java/com/lcc/naildemo/NailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lcc.naildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NailDemoApplication.class, args);
+    }
+
+}
